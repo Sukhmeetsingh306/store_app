7839d0ceab4e4e20ebaaaa5e0ebdbc96a50f7378
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-23 00:22:46 +0530

    made the function to have access at one place

--- lib/components/text/textFormField.dart
+++ lib/components/text/textFormField.dart
@@ -1,22 +1,31 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 
 import '../color/color_theme.dart';
 import 'googleFonts.dart';
 
-Padding textFormField(String labelText, String image) {
+Padding textFormField(
+  String labelText,
+  String image, {
+  bool? obscureText,
+  IconButton? passObscureText,
+}) {
   return Padding(
     padding: const EdgeInsets.all(12.0),
     child: TextFormField(
+      obscureText: obscureText ?? false,
       decoration: InputDecoration(
         fillColor: ColorTheme.color.whiteColor,
         filled: true,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(9),
-          borderSide: BorderSide(color: ColorTheme.color.lustRedColor),
+          borderSide: BorderSide(
+            color: ColorTheme.color.lustRedColor,
+          ),
         ),
         labelText: labelText,
-        //focusedBorder: InputBorder.none,
-        //enabledBorder: InputBorder.none,
+        errorText: 'Invalid input',
         labelStyle: googleFonts(
           fontFamily: 'Nunito Sans',
           fontSize: 14,
@@ -30,6 +39,7 @@ Padding textFormField(String labelText, String image) {
             height: 20,
           ),
         ),
+        suffixIcon: passObscureText, // Use the passed IconButton directly
       ),
     ),
   );
