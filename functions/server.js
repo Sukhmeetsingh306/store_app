b79a672dbd7d5450e900fa5a5ff965a443edec0e
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-23 16:11:14 +0530

    importing the home route as in main code

--- functions/server.js
+++ functions/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
+import {homeRoute} from './routes/home.js';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 
-app.route('/')
-     .get((req, res)=>{
-          res.send('Hello Working');
-     })
+app.use(homeRoute);
 
 app.listen(port,()=>{
      console.log(`Server is running on port ${port}`);
