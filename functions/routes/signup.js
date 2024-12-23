422a303656391d5aa57517b28d77281b7f66f5a0
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-23 19:25:08 +0530

    Added validation for the code name and password

--- functions/routes/signup.js
+++ functions/routes/signup.js
@@ -13,27 +13,42 @@ const userSchema = new mongoose.Schema({
     trim: true,
     lowercase: true,
     validate:{validator:(value)=>{
-     const result = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
-     return result.test(value);
-    }}
+        const result = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+        return result.test(value);
+        },
+        message: 'Please enter a valid email address.'
+      }
     },
 
     state: {
-     type: String,
-     default: '',
+      type: String,
+      default: '',
     },
-     city: {
-          type: String,
-          default: '',
-     },
-     locality: {
-          type: String,
-          default: '',
-     },
+
+    city: {
+      type: String,
+      default: '',
+    },
+
+    locality: {
+      type: String,
+      default: '',
+    },
 
   password: {
     type: String,
     required: true,
+    validate:{
+      validator:(value)=>{
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if(value.length <=8){
+          message: 'Password must be at least 8 characters long.'
+        }else if(value.contain(passwordRegex)){
+          message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+        }
+        
+      },
+    }
   },
 }, { timestamps: true });
 
