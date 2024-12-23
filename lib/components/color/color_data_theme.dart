61d764568310df84f7daf33438d47329407f074c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-23 11:40:26 +0530

    change color code to hex

--- lib/components/color/color_data_theme.dart
+++ lib/components/color/color_data_theme.dart
@@ -1,19 +1,16 @@
-
 import 'package:flutter/material.dart';
 
 @immutable
-class ColorThemeData{
-
+class ColorThemeData {
   final whiteColor = const Color(0xffFFFFFF);
   final lustRedColor = const Color(0xffE11717);
   final transparentBack = Colors.transparent;
   final backgroundBlack = const Color.fromRGBO(15, 15, 14, 0.5);
-  final deepPuceColor =  const Color(0xffAC5F5F);
+  final deepPuceColor = const Color(0xffAC5F5F);
   final blackColor = const Color(0xff000000);
   final grayColor = const Color(0xff9A9A9A);
   final blueColor = const Color(0xff0000FF);
-  final verySoftCyan = const Color(0xB2F3F3);
-
+  final verySoftCyan = const Color(0x00b2f3f3);
 
   const ColorThemeData();
 }
