11d8bfaf56113af6c5f513bf1ae6d707e6a048a5
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-24 00:49:15 +0530

    fixed the errors in the code for adding the new users

--- functions/routes/auth.js
+++ functions/routes/auth.js
@@ -4,20 +4,24 @@ import User from "../models/userInf.js"
 const appAuthSignIn= express();
 
 appAuthSignIn.route('/api/signup')
-     .post(async(req, res)=>{
-          try{
-               const {name, email, password} = req.body;
-               const existingUser = await User.findOne({email}); // that finding that there is already an existing user
+     .post(async (req, res) => {
+          try {
+          const { name, email, password } = req.body;
 
-               if(existingUser){
-                    return res.status(400).json({message: 'User already exists'});
+          if (!name || !email || !password) {
+               return res.status(400).json({ message: `All fields are required: ${name}, ${email}, ${password}` });
+          }
+          const existingUser = await User.findOne({ email });
+               if (existingUser) {
+                    return res.status(400).json({ message: 'User already exists' });
                }else{
-                    const newUser = new User({name, email, password});
+                    const newUser = new User({ name, email, password });
                     await newUser.save();
-                    res.status(201).json({newUser: newUser});
+                    res.status(201).json({ newUser });
                }
-          }catch(err){
-               res.status(500).json({message: err.message});
+          } catch (err) {
+          //console.error('Error in saving user:', err);
+          res.status(500).json({ message: err.message });
           }
      });
 
