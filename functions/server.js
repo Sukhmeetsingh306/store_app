032b4a4d26bbeb73a229569609700dcbf5e6d874
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-23 16:03:57 +0530

    printed hello in the route structure

--- functions/server.js
+++ functions/server.js
@@ -4,7 +4,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-console.log('hello')
+
+app.route('/')
+     .get((req, res)=>{
+          res.send('Hello Working');
+     })
 
 app.listen(port,()=>{
      console.log(`Server is running on port ${port}`);
