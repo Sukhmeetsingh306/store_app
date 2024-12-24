b7de47d5173a22331a0407a62f31359df1904321
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-24 14:48:23 +0530

    adding the salting and hashing in the password

--- functions/routes/auth.js
+++ functions/routes/auth.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import User from "../models/userInf.js"
 
 const appAuthSignIn= express();
@@ -15,7 +16,9 @@ appAuthSignIn.route('/api/signup')
                if (existingUser) {
                     return res.status(400).json({ message: 'User already exists' });
                }else{
-                    const newUser = new User({ name, email, password });
+                    const salt = await bcrypt.genSalt(12);
+                    const hashedPassword = await bcrypt.hash(password, salt);
+                    const newUser = new User({ name, email, password: hashedPassword });
                     await newUser.save();
                     res.status(201).json({ newUser });
                }
