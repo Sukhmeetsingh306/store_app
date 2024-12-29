fbdaf67f26ff45f71d4a5ca0d6941d2b4aa7c5c7
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-29 22:03:58 +0530

    added the bottom navigation in the code

---
+++ lib/views/screens/main_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:store_app/models/image_model.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          bottomBarItem(
+            Icon(Icons.home_outlined),
+            'Home',
+          ),
+          bottomBarItem(
+            Icon(Icons.favorite_border_outlined),
+            'Favorite',
+          ),
+          bottomBarItem(
+            Icon(Icons.shopping_bag_outlined),
+            'Stores',
+          ),
+          bottomBarItem(
+            Icon(Icons.shopping_cart_outlined),
+            'Cart',
+          ),
+          bottomBarItem(
+            imageAssets('assets/images/bottomBarImages/user.png'),
+            'Account',
+          ),          
+        ],
+        elevation: 5,
+      ),
+    );
+  }
+}
