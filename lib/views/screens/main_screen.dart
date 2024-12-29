f3d1bb089e10d4944faf07131de459d37419f659
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-29 22:15:07 +0530

    updated the style of the bottombar

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/components/color/color_theme.dart';
 import 'package:store_app/models/image_model.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
+        currentIndex: _pageIndex,
+        selectedItemColor: ColorTheme.color.deepPuceColor,
+        selectedFontSize: 14,
+        unselectedFontSize: 12,
+        unselectedItemColor: ColorTheme.color.grayColor,
+        onTap: (value) => setState(
+          () {
+            _pageIndex = value;
+          },
+        ),
         items: [
           bottomBarItem(
             Icon(Icons.home_outlined),
@@ -27,9 +44,9 @@ class MainScreen extends StatelessWidget {
             'Cart',
           ),
           bottomBarItem(
-            imageAssets('assets/images/bottomBarImages/user.png'),
+            Icon(Icons.account_circle_outlined),
             'Account',
-          ),          
+          ),
         ],
         elevation: 5,
       ),
