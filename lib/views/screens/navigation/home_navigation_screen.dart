f7ca7a86421b5df0b57dadbb3577515d365974f6
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-29 22:24:49 +0530

    added code for testing

--- lib/views/screens/navigation/home_navigation_screen.dart
+++ lib/views/screens/navigation/home_navigation_screen.dart
@@ -5,6 +5,10 @@ class HomeNavigationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: const Center(
+        child: Text("home"),
+      ),
+    );
   }
 }
