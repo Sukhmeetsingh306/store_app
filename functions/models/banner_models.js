ac0b5f7ae963129f9119d6d114f93de46634282d
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-30 13:25:12 +0530

    create the module for the banner as adding the mongoose to connect with the db

---
+++ functions/models/banner_models.js
@@ -0,0 +1,12 @@
+import mongoose, { mongo } from "mongoose";
+
+const bannerSchema = mongoose.Schema({
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Banner = mongoose.model("Banner", bannerSchema);
+
+export default Banner;
