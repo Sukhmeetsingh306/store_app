6c92c81707624d99005f9db4f99732d0f521accd
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-30 22:04:25 +0530

    created the get request and it is working

--- functions/routes/banner_routes.js
+++ functions/routes/banner_routes.js
@@ -3,17 +3,30 @@ import Banner from "../models/banner_models.js";
 
 const bannerRouter = express.Router();
 
-bannerRouter.route("/api/banner").post(async (req, res) => {
-  try {
-    const { image } = req.body;
-    const banner = new Banner({ image });
-    await banner.save();
-    console.log("banner ", banner, "success");
-    return res.status(201).json(banner);
-  } catch (e) {
-    console.log("error in banner");
-    return res.status(400).json({ error: e.message });
-  }
-});
+bannerRouter
+  .route("/api/banner")
+  .post(async (req, res) => {
+    try {
+      const { image } = req.body;
+      const banner = new Banner({ image });
+      await banner.save();
+      console.log("banner ", banner, "success");
+      return res.status(201).json(banner);
+    } catch (e) {
+      console.log("error in banner");
+      return res.status(400).json({ error: e.message });
+    }
+  })
+
+  .get(async (req, res) => {
+    try {
+      const banner = await Banner.find();
+      console.log("banner ", banner, "success in get");
+      return res.status(200).json(banner);
+    } catch (e) {
+      console.log("error in banner in get request");
+      return res.status(400).json({ error: e.message });
+    }
+  });
 
 export { bannerRouter };
