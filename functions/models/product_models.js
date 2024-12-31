50ffc2c836d3f82898aa4c7763689d2f52e9fa26
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-01 00:05:47 +0530

    created the model for the product

---
+++ functions/models/product_models.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+     productName:{
+          type: String,
+          required: true,
+          trim: true,
+     },
+
+     productPrice:{
+          type: Number,
+          required: true,
+     },
+
+     productQuantity: {
+          type: Number,
+          required: true,
+     },
+
+     productDescription: {
+          type: String,
+          required: true,
+          trim: true,
+     },
+
+     productCategory: {
+          type: String,
+          required: true,
+     },
+
+     productSubCategory: {
+          type: String,
+          required: true,
+     },
+
+     productImage: [
+          {
+               type: String,
+               required: true,
+          }
+     ],
+
+     productPopularity:{
+          type: Number,
+          default: 0,
+     },
+
+     productRecommended:{
+          type: Boolean,
+          default: false,
+     }
+});
+
+const Product = mongoose.model('Product', productSchema);
+export  { Product };
