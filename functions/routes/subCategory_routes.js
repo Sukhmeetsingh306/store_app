acc98ef7f06e757bfce1e4405298f0aca6515fd7
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2024-12-31 21:42:49 +0530

    created the post request for subCategory

---
+++ functions/routes/subCategory_routes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { SubCategory } from "../models/subCategory_models";
+
+const subCategoryRouter = express.Router();
+
+subCategoryRouter.route("/api/subCategory").post(async (req, res) => {
+  try {
+    const { categoryId, categoryName, image, subCategoryName } = req.body;
+    const newSubCategory = new SubCategory({
+      categoryId,
+      categoryName,
+      image,
+      subCategoryName,
+    });
+    await newSubCategory.save();
+    console.log("SubCategory", newSubCategory, "saved successfully");
+    return res.status(201).json(newSubCategory);
+  } catch (e) {
+    console.log("Error in creating SubCategory", e);
+    return res.status(400).json({ error: e.message });
+  }
+});
+
+export { subCategoryRouter };
