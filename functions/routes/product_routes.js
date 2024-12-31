1a3438678ea74724fe51931d520f85029c051774
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-01 00:51:55 +0530

    created the get request for the recommended-product

--- functions/routes/product_routes.js
+++ functions/routes/product_routes.js
@@ -36,7 +36,7 @@ productRouter.get("/api/popular-product", async (req, res) => {
   try {
     const popularProducts = await Product.find({ productPopularity: true });
     if (!popularProducts || popularProducts.length == 0) {
-     console.log("No popular products")
+      console.log("No popular products");
       return res.status(404).json({
         message: "No popular products found",
       });
@@ -46,6 +46,26 @@ productRouter.get("/api/popular-product", async (req, res) => {
     }
   } catch (e) {
     console.log("Error in getting popular products", e);
+    return res.status(400).json({ error: e.message });
+  }
+});
+
+productRouter.get("/api/recommended-product", async (req, res) => {
+  try {
+    const recommendedProducts = await Product.find({
+      productRecommended: true,
+    });
+    if (!recommendedProducts || recommendedProducts.length == 0) {
+      console.log("No recommended products");
+      return res.status(404).json({
+        message: "No recommended products found",
+      });
+    } else {
+      console.log("Recommended Products are", recommendedProducts);
+      return res.status(200).json(recommendedProducts);
+    }
+  } catch (e) {
+    console.log("Error in getting recommended products", e);
     return res.status(400).json({ error: e.message });
   }
 });
