c71ec55fbe98f8a133f9ad7fef33f387ca017105
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-01 00:16:38 +0530

    created the product route as adding accessing the model with it

---
+++ functions/routes/product_routes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { Product } from "../models/product_models.js";
+
+const productRouter = express.Router();
+
+productRouter.route("/api/add-product").post(async (req, res) => {
+  try {
+    const {
+      productName,
+      productPrice,
+      productQuantity,
+      productDescription,
+      productCategory,
+      productSubCategory,
+      productImage,
+    } = req.body;
+    const newProduct = new Product({
+      productName,
+      productPrice,
+      productQuantity,
+      productDescription,
+      productCategory,
+      productSubCategory,
+      productImage,
+    });
+    await newProduct.save();
+    console.log("Product", newProduct, "saved successfully");
+    return res.status(201).json(newProduct);
+  } catch (e) {
+    console.log("Error in creating product", e);
+    return res.status(400).json({ error: e.message });
+  }
+});
+
+export { productRouter };
