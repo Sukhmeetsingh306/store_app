85d8800bd3ff42d4b2873c0664ee0172a1f251b7
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-01 15:43:27 +0530

    commented the validator as to check after the pre-check

--- functions/models/rating_review_models.js
+++ functions/models/rating_review_models.js
@@ -1,41 +1,53 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const ratingReviewSchema = mongoose.Schema({
-     buyerId: {
-          type: String,
-          required: true,
-          unique: true // self added if any error get known
-     },
+  buyerId: {
+    type: String,
+    required: true,
+    unique: true, // self added if any error get known
+  },
 
-     buyerEmail: {
-          type: String,
-          required: true,
-          unique: true,
-          lowercase: true,
-     },
+  buyerEmail: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+//     validate: {
+//       validator: (value) => {
+//         const result =
+//           /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+//         return result.test(value);
+//       },
+//       message: "Please enter a valid email address.",
+//     },
+  },
 
-     buyerFullName: {
-          type: String,
-          required: true,
-          trim: true,
-     },
+  buyerFullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-     productId: { // as each product will have there own id and this will match form the product table
-          type: String,
-          required: true,
-          unique: true,
-     },
+  productId: {
+    // as each product will have there own id and this will match form the product table
+    type: String,
+    required: true,
+    unique: true,
+  },
 
-     productRating:{
-          type: Number,
-          default: 0,
-     },
+  productRating: {
+    type: Number,
+    default: 0,
+  },
 
-     productReview:{
-          type: String,
-          trim: true,
-     }
+  productReview: {
+    type: String,
+    trim: true,
+  },
 });
 
-const ProductRatingReview = mongoose.model('ProductRatingReview', ratingReviewSchema);
+const ProductRatingReview = mongoose.model(
+  "ProductRatingReview",
+  ratingReviewSchema
+);
 export { ProductRatingReview };
