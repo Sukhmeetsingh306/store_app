2629c11a42ac21169df22d8f528149999016e75c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-01 15:41:48 +0530

    route created for the rating review

---
+++ functions/routes/rating_review_routes.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { ProductRatingReview } from "../models/rating_review_models.js";
+
+const productRatingReviewRouter = express.Router();
+
+productRatingReviewRouter.post(
+  "/api/product-rating-review",
+  async (req, res) => {
+    const {
+      buyerId,
+      buyerEmail,
+      buyerFullName,
+      productId,
+      productRating,
+      productReview,
+    } = req.body;
+
+    try {
+      const productRatingReview = new ProductRatingReview({
+        buyerId,
+        buyerEmail,
+        buyerFullName,
+        productId,
+        productRating,
+        productReview,
+      });
+      await productRatingReview.save();
+      console.log(
+        "Product rating review",
+        productRatingReview,
+        " saved successfully"
+      );
+      return res.status(201).json(productRatingReview);
+    } catch (e) {
+      console.log("Error in saving product rating review", e);
+      return res.status(400).json({ error: e.message });
+    }
+  }
+);
