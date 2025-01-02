d8222e2b5f4cb28fc1a59fcd140ecab48352bbd4
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-02 22:51:18 +0530

    created all the files and there route for the management

---
+++ lib/views/screens/side_screens/upload_banner_side_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class UploadBannerSideScreen extends StatelessWidget {
+  static const String routeName = '/uploadBannerScreen';
+  const UploadBannerSideScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'UploadBannerSideScreen',
+      ),
+    );
+  }
+}
