b704931d5746c21efcbf74bc8f088c1033df9577
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-02 23:29:00 +0530

    added the routes to the menu items

--- lib/views/device/web_device_view.dart
+++ lib/views/device/web_device_view.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+import 'package:store_app/views/screens/side_screens/buyer_side_screen.dart';
+import 'package:store_app/views/screens/side_screens/category_side_screen.dart';
+import 'package:store_app/views/screens/side_screens/order_side_screen.dart';
+import 'package:store_app/views/screens/side_screens/product_side_screen.dart';
+import 'package:store_app/views/screens/side_screens/upload_banner_side_screen.dart';
 
 import '../../components/color/color_theme.dart';
 import '../../components/text/googleFonts.dart';
+import '../screens/side_screens/vendor_side_screen.dart';
 
 class WebDeviceView extends StatefulWidget {
   const WebDeviceView({super.key});
@@ -13,45 +19,46 @@ class WebDeviceView extends StatefulWidget {
 }
 
 class _WebDeviceViewState extends State<WebDeviceView> {
-  @override
-  Widget build(BuildContext context)  {
+  final Widget _selectedScreen = VendorSideScreen();
+
+  Widget web(){
     return AdminScaffold(
       appBar: AppBar(
         backgroundColor: ColorTheme.color.dodgerBlue,
         title: googleText("Management"),
         centerTitle: false,
       ),
-      body: googleText('DashBoard'),
+      body: _selectedScreen,
       sideBar: SideBar(
         items: const [
           AdminMenuItem(
             title: 'Vendors',
-            route: "",
+            route: VendorSideScreen.routeName,
             icon: CupertinoIcons.person_3,
           ),
           AdminMenuItem(
             title: 'Buyers',
-            route: "",
+            route: BuyerSideScreen.routeName,
             icon: CupertinoIcons.person,
           ),
           AdminMenuItem(
             title: 'Orders',
-            route: '',
+            route: OrderSideScreen.routeName,
             icon: Icons.shopping_cart_outlined,
           ),
           AdminMenuItem(
             title: 'Categories',
-            route: "",
+            route: CategorySideScreen.routeName,
             icon: Icons.category_outlined,
           ),
           AdminMenuItem(
             title: 'Upload Banners',
-            route: "",
+            route: UploadBannerSideScreen.routeName,
             icon: Icons.upload_sharp,
           ),
           AdminMenuItem(
             title: 'Products',
-            route: '',
+            route: ProductSideScreen.routeName,
             icon: Icons.shopping_cart_outlined,
           ),
         ],
@@ -63,5 +70,10 @@ class _WebDeviceViewState extends State<WebDeviceView> {
         iconColor: Colors.black,
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context)  {
+    return web();
   }
 }
