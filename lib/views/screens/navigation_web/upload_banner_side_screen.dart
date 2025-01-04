c924115f833fcf7412e1f021cb112b875bda6012
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-05 01:01:50 +0530

    created the first part

--- lib/views/screens/navigation_web/upload_banner_side_screen.dart
+++ lib/views/screens/navigation_web/upload_banner_side_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/cupertino.dart';
 
+import '../../../components/code/divider_code.dart';
+import '../../../components/code/text/googleFonts.dart';
+
 class UploadBannerSideScreen extends StatelessWidget {
   static const String routeName = '/uploadBannerScreen';
   const UploadBannerSideScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'UploadBannerSideScreen',
-      ),
+    return Column(
+      children: [Container(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: googleText(
+                  'Banner',
+                  fontSize: 36,
+                ),
+              ),
+            ),
+            divider(),],
     );
   }
 }
