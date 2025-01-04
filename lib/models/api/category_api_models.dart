97e1788404704ce6c6ca17c6ae6fd66caa5e2730
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-04 12:46:19 +0530

    created the map and convert it to json form

--- lib/models/api/category_api_models.dart
+++ lib/models/api/category_api_models.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Category {
   final String categoryId;
   final String categoryName;
@@ -10,4 +12,31 @@ class Category {
     required this.categoryImage,
     required this.categoryBanner,
   });
+
+  Map<String, dynamic> categoryToMap() {
+    // creating a map
+    return <String, dynamic>{
+      'categoryId': categoryId,
+      'categoryName': categoryName,
+      'categoryImage': categoryImage,
+      'categoryBanner': categoryBanner,
+    };
+  }
+
+  factory Category.categoryFromMap(Map<String, dynamic> map) {
+    //getting from the map
+    return Category(
+      categoryId: map['categoryId'] as String,
+      categoryName: map['categoryName'] as String,
+      categoryImage: map['categoryImage'] as String,
+      categoryBanner: map['categoryBanner'] as String,
+    );
+  }
+
+  String categoryToJson() =>
+      json.encode(categoryToMap()); // coding the map to the json
+
+  factory Category.categoryFromJson(String source) => Category.categoryFromMap(
+        json.decode(source) as Map<String, dynamic>,
+      );
 }
