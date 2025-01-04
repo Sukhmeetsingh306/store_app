4b0618303f5573ef11fe464bca2b59c9f05c4705
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-04 12:14:50 +0530

    add the element that would be required in the code

---
+++ lib/models/api/category_api_models.dart
@@ -0,0 +1,13 @@
+class Category {
+  final String categoryId;
+  final String categoryName;
+  final String categoryImage;
+  final String categoryBanner;
+
+  Category({
+    required this.categoryId,
+    required this.categoryName,
+    required this.categoryImage,
+    required this.categoryBanner,
+  });
+}
