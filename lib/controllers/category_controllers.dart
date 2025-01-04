def3a1babd5ee390ca99a0b10c8872d52b057d1c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-04 18:55:28 +0530

    imported the model in the controller and converting it to the post

--- lib/controllers/category_controllers.dart
+++ lib/controllers/category_controllers.dart
@@ -1,9 +1,16 @@
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:store_app/globals_variables.dart';
+import 'package:store_app/models/api/category_api_models.dart';
+
+import 'package:http/http.dart' as http;
+
 
 class CategoryControllers {
-  uploadCategory(
-      {required dynamic pickedImage, required dynamic pickedBanner}) async {
+  uploadCategory({
+    required dynamic pickedImage,
+    required dynamic pickedBanner,
+    required String categoryName,
+  }) async {
     try {
       final categoryCloudinary = CloudinaryPublic(
         cloudinaryName,
@@ -25,8 +32,16 @@ class CategoryControllers {
         ),
       );
 
-      print(categoryImageResponse);
-      print(categoryBannerResponse);
+      String categoryImage = categoryImageResponse.secureUrl;
+
+      String categoryBannerImage = categoryBannerResponse.secureUrl;
+
+      Category category = Category(
+        categoryId: "",
+        categoryName: categoryName,
+        categoryImage: categoryImage,
+        categoryBanner: categoryBannerImage,
+      );
     } catch (e) {
       print("Error uploading in cloudinary in category_controller : $e");
     }
