a4b18ecba6b6c25e1fccc164f8677e280e4d5669
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-04 19:03:29 +0530

    handled the http request from the code

--- lib/controllers/category_controllers.dart
+++ lib/controllers/category_controllers.dart
@@ -1,15 +1,17 @@
+import 'dart:convert';
+
 import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:flutter/material.dart';
 import 'package:store_app/globals_variables.dart';
 import 'package:store_app/models/api/category_api_models.dart';
 
 import 'package:http/http.dart' as http;
-
-
 class CategoryControllers {
   uploadCategory({
     required dynamic pickedImage,
     required dynamic pickedBanner,
     required String categoryName,
+    required context,
   }) async {
     try {
       final categoryCloudinary = CloudinaryPublic(
@@ -42,6 +44,49 @@ class CategoryControllers {
         categoryImage: categoryImage,
         categoryBanner: categoryBannerImage,
       );
+
+      http.Response response = await http.post(
+          Uri.parse("$webUri/api/category"),
+          body: category.categoryToJson(),
+          headers: <String, String>{
+            "content-type": "application/json; charset=UTF-8",
+          }).timeout(const Duration(seconds: 10), onTimeout: () {
+        throw Exception('Request timed out');
+      });
+
+      // Handling the response
+      if (response.statusCode == 200) {
+        // Handle success - Parse the JSON response
+        var responseData = jsonDecode(response.body);
+        String token = responseData['token'];
+        // You can store the token or navigate the user to another screen
+        print("Category Uploaded successful: Token: $token");
+        // Navigate to another screen or do something else
+      } else {
+        // Show error message if response is not 200
+        var responseData = jsonDecode(response.body);
+        String errorMessage = responseData['message'];
+        print('Error: $errorMessage');
+
+        // Show AlertDialog with the error message
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Error'),
+              content: Text(errorMessage),
+              actions: [
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
     } catch (e) {
       print("Error uploading in cloudinary in category_controller : $e");
     }
