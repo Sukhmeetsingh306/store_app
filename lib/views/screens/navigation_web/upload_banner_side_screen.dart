3be9c298f61492723f6a69e30b9d65523097e993
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-05 13:54:13 +0530

    code created as per ui of the banner upper part of the page

--- lib/views/screens/navigation_web/upload_banner_side_screen.dart
+++ lib/views/screens/navigation_web/upload_banner_side_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 
+import '../../../components/code/button_code.dart';
 import '../../../components/code/divider_code.dart';
+import '../../../components/code/sized_space_code.dart';
 import '../../../components/code/text/googleFonts.dart';
+import '../../../components/code/webImageInput_code.dart';
 
 class UploadBannerSideScreen extends StatefulWidget {
   static const String routeName = '/uploadBannerScreen';
@@ -12,23 +15,59 @@ class UploadBannerSideScreen extends StatefulWidget {
 }
 
 class _UploadBannerSideScreenState extends State<UploadBannerSideScreen> {
+  dynamic _uploadBannerImage;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.topLeft,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: googleText(
-              'Banner',
-              fontSize: 36,
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: googleText(
+                'Banner',
+                fontSize: 36,
+              ),
             ),
           ),
-        ),
-        divider(),
-        Row(children: [],),
-      ],
+          divider(),
+          Row(
+            children: [
+              webImageInput(
+                _uploadBannerImage,
+                "Banner Image",
+                context,
+              ),
+              sizedBoxMediaQuery(
+                context,
+                width: 0.023,
+                height: 0,
+              ),
+              elevatedButton(
+                () {},
+                "Submit",
+              ),
+            ],
+          ),
+          sizedBoxMediaQuery(
+            context,
+            width: 0,
+            height: 0.02,
+          ),
+          elevatedButton(
+            () {},
+            "Upload Image",
+          ),
+          divider(),
+        ],
+      ),
     );
   }
 }
