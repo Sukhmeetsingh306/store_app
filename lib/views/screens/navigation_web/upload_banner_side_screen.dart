a3e84e223e04d4003c26a4a3c3f024025f3e3406
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-05 13:42:32 +0530

    convert the widget to the statefull widget

--- lib/views/screens/navigation_web/upload_banner_side_screen.dart
+++ lib/views/screens/navigation_web/upload_banner_side_screen.dart
@@ -3,24 +3,32 @@ import 'package:flutter/cupertino.dart';
 import '../../../components/code/divider_code.dart';
 import '../../../components/code/text/googleFonts.dart';
 
-class UploadBannerSideScreen extends StatelessWidget {
+class UploadBannerSideScreen extends StatefulWidget {
   static const String routeName = '/uploadBannerScreen';
   const UploadBannerSideScreen({super.key});
 
   @override
+  State<UploadBannerSideScreen> createState() => _UploadBannerSideScreenState();
+}
+
+class _UploadBannerSideScreenState extends State<UploadBannerSideScreen> {
+  @override
   Widget build(BuildContext context) {
     return Column(
-      children: [Container(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: googleText(
-                  'Banner',
-                  fontSize: 36,
-                ),
-              ),
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: googleText(
+              'Banner',
+              fontSize: 36,
             ),
-            divider(),],
+          ),
+        ),
+        divider(),
+        Row(children: [],),
+      ],
     );
   }
 }
