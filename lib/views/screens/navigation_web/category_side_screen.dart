c86f20fcde993c4b0f0e868427c78e397a5cde19
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-19 01:19:53 +0530

    reload the page when submit is clicked

--- lib/views/screens/navigation_web/category_side_screen.dart
+++ lib/views/screens/navigation_web/category_side_screen.dart
@@ -18,13 +18,19 @@ class CategorySideScreen extends StatefulWidget {
 }
 
 class _CategorySideScreenState extends State<CategorySideScreen> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final CategoryControllers _categoryController = CategoryControllers();
 
   late String categoryName;
 
   dynamic _categoryImage;
   dynamic _bannerImage;
+
+  bool _isLoading = false;
+
+  Future<dynamic> simulateImageUpload() async {
+    await Future.delayed(Duration(seconds: 2));
+  }
 
   Future<void> uploadImage(ValueSetter<dynamic> updateImage) async {
     FilePickerResult? fileImage = await FilePicker.platform.pickFiles(
@@ -50,120 +56,153 @@ class _CategorySideScreenState extends State<CategorySideScreen> {
     );
   }
 
+  void reloadWidget() {
+    setState(() {
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     double mediaQueryWidth = MediaQuery.of(context).size.width;
 
-    return Form(
-      key: _formKey,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 4,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: googleText(
-                    'Category',
-                    fontSize: 36,
-                  ),
-                ),
+    return Stack(
+      children: [
+        Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
               ),
-              divider(),
-              Row(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  webImageInput(
-                    _categoryImage,
-                    'Category Image',
-                    context,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      width: mediaQueryWidth * 0.15,
-                      child: TextFormField(
-                        onChanged: (value) {
-                          categoryName = value;
-                        },
-                        validator: (value) {
-                          if (value == null ||
-                              value.isEmpty ||
-                              value.length <= 3) {
-                            return 'Please Enter Valid Category Name';
-                          }
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                          labelText: 'Enter Category Name',
-                        ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: googleText(
+                        'Category',
+                        fontSize: 36,
                       ),
                     ),
                   ),
+                  divider(),
+                  Row(
+                    children: [
+                      webImageInput(
+                        _categoryImage,
+                        'Category Image',
+                        context,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          width: mediaQueryWidth * 0.15,
+                          child: TextFormField(
+                            onChanged: (value) {
+                              categoryName = value;
+                            },
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  value.length <= 3) {
+                                return 'Please Enter Valid Category Name';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Enter Category Name',
+                            ),
+                          ),
+                        ),
+                      ),
+                      sizedBoxMediaQuery(
+                        context,
+                        width: 0.023,
+                        height: 0,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: webButtonGoogleText(
+                          'Cancel',
+                        ),
+                      ),
+                      elevatedButton(
+                        () async {
+                          setState(() {
+                            _isLoading = true;
+                          });
+                          if (_formKey.currentState!.validate()) {
+                            print(categoryName);
+                            _categoryController.uploadCategory(
+                              pickedImage: _categoryImage,
+                              pickedBanner: _bannerImage,
+                              categoryName: categoryName,
+                              context: context,
+                            );
+                          } // will change it when the api creation will begin
+                          dynamic newImage = await simulateImageUpload();
+                          setState(() {
+                            _categoryImage = newImage;
+                            _isLoading = false;
+                          });
+                          reloadWidget();
+                        },
+                        "Submit",
+                      ),
+                    ],
+                  ),
                   sizedBoxMediaQuery(
                     context,
-                    width: 0.023,
-                    height: 0,
+                    width: 0,
+                    height: 0.02,
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: webButtonGoogleText(
-                      'Cancel',
-                    ),
-                  ),
-                  elevatedButton(
-                    () async {
-                      if (_formKey.currentState!.validate()) {
-                        print(categoryName);
-                        _categoryController.uploadCategory(
-                          pickedImage: _categoryImage,
-                          pickedBanner: _bannerImage,
-                          categoryName: categoryName,
-                          context: context,
-                        );
-                      } // will change it when the api creation will begin
+                  elevatedButtonCategory(
+                    (image) {
+                      _categoryImage = image;
                     },
-                    "Submit",
                   ),
+                  divider(),
+                  webImageInput(
+                    _bannerImage,
+                    'Banner Image',
+                    context,
+                  ),
+                  sizedBoxMediaQuery(
+                    context,
+                    width: 0,
+                    height: 0.02,
+                  ),
+                  elevatedButtonCategory(
+                    (image) {
+                      _bannerImage = image;
+                    },
+                  ),
+                  divider(),
+                  CategoryWidget(),
                 ],
               ),
-              sizedBoxMediaQuery(
-                context,
-                width: 0,
-                height: 0.02,
-              ),
-              elevatedButtonCategory(
-                (image) {
-                  _categoryImage = image;
-                },
-              ),
-              divider(),
-              webImageInput(
-                _bannerImage,
-                'Banner Image',
-                context,
-              ),
-              sizedBoxMediaQuery(
-                context,
-                width: 0,
-                height: 0.02,
-              ),
-              elevatedButtonCategory(
-                (image) {
-                  _bannerImage = image;
-                },
-              ),
-              divider(),
-              CategoryWidget(),
-            ],
+            ),
           ),
         ),
-      ),
+        if (_isLoading)
+          Positioned.fill(
+            child: Stack(
+              children: [
+                ModalBarrier(
+                  dismissible: false,
+                  color: Colors.black.withValues(alpha: .5),
+                ),
+                Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ],
+            ),
+          ),
+      ],
     );
   }
 }
