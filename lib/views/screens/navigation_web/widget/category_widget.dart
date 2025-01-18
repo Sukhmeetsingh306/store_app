bb05bcd5c5f1cc6764d4852bb28d935801d57c64
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-19 00:40:11 +0530

    created the code for viewing the created category

--- lib/views/screens/navigation_web/widget/category_widget.dart
+++ lib/views/screens/navigation_web/widget/category_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/models/api/category_api_models.dart';
 
+import '../../../../components/code/text/googleFonts.dart';
 import '../../../../controllers/category_controllers.dart';
 
 class CategoryWidget extends StatefulWidget {
@@ -11,7 +12,6 @@ class CategoryWidget extends StatefulWidget {
 }
 
 class _CategoryWidgetState extends State<CategoryWidget> {
-
   late Future<List<CategoryApiModels>> futureCategory;
 
   @override
@@ -22,6 +22,50 @@ class _CategoryWidgetState extends State<CategoryWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: futureCategory,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return errormessage("Error: ${snapshot.error}");
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(
+            child: googleText(
+              "No banners found",
+              fontWeight: FontWeight.normal,
+              fontSize: 18,
+            ),
+          );
+        } else {
+          final categoryCount = snapshot.data!;
+          return GridView.builder(
+            itemCount: categoryCount.length,
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 6,
+              crossAxisSpacing: 15,
+              mainAxisSpacing: 8,
+            ),
+            itemBuilder: (context, index) {
+              final category = categoryCount[index];
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network(
+                      width: 100,
+                      height: 10,
+                      category.categoryImage,
+                    ),
+                  ),
+                  googleText(category.categoryName),
+                ],
+              );
+            },
+          );
+        }
+      },
+    );
   }
 }
