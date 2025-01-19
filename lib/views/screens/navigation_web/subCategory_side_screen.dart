5a0d00581c8bc1783495249df78bdca5fd565995
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-19 16:01:48 +0530

    starting code as of the field of subcategory

--- lib/views/screens/navigation_web/subCategory_side_screen.dart
+++ lib/views/screens/navigation_web/subCategory_side_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../../components/code/divider_code.dart';
+import '../../../components/code/text/googleFonts.dart';
+
 class SubCategorySideScreen extends StatelessWidget {
   static const String routeName = '/subCategoryScreen';
 
@@ -7,8 +10,27 @@ class SubCategorySideScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Subcategory"),
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: googleText(
+                'Subcategory',
+                fontSize: 36,
+              ),
+            ),
+          ),
+          divider(),
+        ],
+      ),
     );
   }
 }
