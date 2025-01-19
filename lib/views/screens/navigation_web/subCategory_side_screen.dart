bd7626dc1f8cde7786959f3756537e3164d79cb9
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-19 22:50:07 +0530

    added the new code for seeing the images

--- lib/views/screens/navigation_web/subCategory_side_screen.dart
+++ lib/views/screens/navigation_web/subCategory_side_screen.dart
@@ -1,7 +1,11 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
+import '../../../components/code/button_code.dart';
 import '../../../components/code/divider_code.dart';
+import '../../../components/code/sized_space_code.dart';
 import '../../../components/code/text/googleFonts.dart';
+import '../../../components/code/webImageInput_code.dart';
 import '../../../controllers/category_controllers.dart';
 import '../../../models/api/category_api_models.dart';
 
@@ -15,9 +19,37 @@ class SubCategorySideScreen extends StatefulWidget {
 }
 
 class _SubCategorySideScreenState extends State<SubCategorySideScreen> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   late Future<List<CategoryApiModels>> futureSubCategory;
 
   CategoryApiModels? selectedCategory;
+
+  late String categoryName;
+
+  dynamic _categoryImage;
+  dynamic _bannerImage;
+
+  bool _isLoading = false;
+  bool _isSnackBarVisible = false;
+
+  Future<dynamic> simulateImageUpload() async {
+    await Future.delayed(Duration(seconds: 2));
+  }
+
+  Future<void> uploadImage(ValueSetter<dynamic> updateImage) async {
+    FilePickerResult? fileImage = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (fileImage != null) {
+      setState(() {
+        updateImage(fileImage
+            .files.first.bytes); // Use the callback to update the image
+        print('Image uploaded');
+      });
+    }
+  }
 
   @override
   void initState() {
@@ -25,69 +57,179 @@ class _SubCategorySideScreenState extends State<SubCategorySideScreen> {
     futureSubCategory = CategoryControllers().fetchCategory();
   }
 
+  void reloadWidget() {
+    setState(() {
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: googleText(
-                'Subcategory',
-                fontSize: 36,
+    double mediaQueryWidth = MediaQuery.of(context).size.width;
+
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: googleText(
+                  'Subcategory',
+                  fontSize: 36,
+                ),
               ),
             ),
-          ),
-          divider(),
-          FutureBuilder(
-            future: futureSubCategory,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return errormessage("Error: ${snapshot.error}");
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Center(
-                  child: googleText(
-                    "No Category found",
-                    fontWeight: FontWeight.normal,
-                    fontSize: 18,
-                  ),
-                );
-              } else {
-                return DropdownButton<CategoryApiModels>(
-                    hint: googleText(
-                      "Select Category",
+            divider(),
+            FutureBuilder(
+              future: futureSubCategory,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return errormessage("Error: ${snapshot.error}");
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(
+                    child: googleText(
+                      "No Category found",
                       fontWeight: FontWeight.normal,
                       fontSize: 18,
                     ),
-                    items: snapshot.data!.map((CategoryApiModels category) {
-                      return DropdownMenuItem<CategoryApiModels>(
-                        value: category,
-                        child: googleText(
-                          category.categoryName,
-                          fontWeight: FontWeight.normal,
-                          fontSize: 16,
-                        ),
-                      );
-                    }).toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        selectedCategory = value;
+                  );
+                } else {
+                  return DropdownButton<CategoryApiModels>(
+                      hint: googleText(
+                        "Select Category",
+                        fontWeight: FontWeight.normal,
+                        fontSize: 18,
+                      ),
+                      items: snapshot.data!.map((CategoryApiModels category) {
+                        return DropdownMenuItem<CategoryApiModels>(
+                          value: category,
+                          child: googleText(
+                            category.categoryName,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 16,
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedCategory = value;
+                        });
+                        print(selectedCategory);
                       });
-                      print(selectedCategory);
+                }
+              },
+            ),
+            Row(
+              children: [
+                webImageInput(
+                  _categoryImage,
+                  'Category Image',
+                  context,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: mediaQueryWidth * 0.15,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        categoryName = value;
+                      },
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length <= 3) {
+                          return 'Please Enter Valid Category Name';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Enter Category Name',
+                      ),
+                    ),
+                  ),
+                ),
+                sizedBoxMediaQuery(
+                  context,
+                  width: 0.023,
+                  height: 0,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: webButtonGoogleText(
+                    'Cancel',
+                  ),
+                ),
+                elevatedButton(
+                  () async {
+                    setState(() {
+                      _isLoading = true;
                     });
-              }
-            },
-          ),
-        ],
+                    if (_bannerImage == null || _categoryImage == null) {
+                      if (!_isSnackBarVisible) {
+                        _isSnackBarVisible = true;
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(
+                              SnackBar(
+                                content: Text('Please add an image'),
+                                duration: Duration(seconds: 2),
+                              ),
+                            )
+                            .closed
+                            .then((_) {
+                          _isSnackBarVisible = false;
+                        });
+                      }
+                      setState(() {
+                        _isLoading = false;
+                      });
+                      return;
+                    }
+                    if (_formKey.currentState!.validate()) {
+
+                      // dynamic newImage = await simulateImageUpload();
+                      // setState(() {
+                      //   _categoryImage = newImage;
+                      //   _bannerImage = newImage;
+                      //   _isLoading = false;
+                      // });
+
+                      reloadWidget(); // Only called when validation passes
+                    } else {
+                      setState(() {
+                        _isLoading = false; // Reset loading if validation fails
+                      });
+                    }
+                  },
+                  "Submit",
+                ),
+              ],
+            ),
+            if (_isLoading)
+              Positioned.fill(
+                child: Stack(
+                  children: [
+                    ModalBarrier(
+                      dismissible: false,
+                      color: Colors.black.withValues(alpha: .5),
+                    ),
+                    Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
