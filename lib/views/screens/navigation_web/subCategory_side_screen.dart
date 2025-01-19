8e47ce00844b9b7133b57ec64d5206636ffdba37
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-19 15:28:49 +0530

    created the new screen for the subcategory

---
+++ lib/views/screens/navigation_web/subCategory_side_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SubCategorySideScreen extends StatelessWidget {
+  static const String routeName = '/subCategoryScreen';
+
+  const SubCategorySideScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Subcategory"),
+    );
+  }
+}
