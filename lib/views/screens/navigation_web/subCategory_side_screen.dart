dfce83d0baecc434502a41ca52d3eadf858d1118
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-20 23:26:32 +0530

    error solved for the assest

--- lib/views/screens/navigation_web/subCategory_side_screen.dart
+++ lib/views/screens/navigation_web/subCategory_side_screen.dart
@@ -1,5 +1,6 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:store_app/controllers/subCategory_controllers.dart';
 import 'package:store_app/views/screens/navigation_web/widget/subCategory_widget.dart';
 
 import '../../../components/code/button_code.dart';
@@ -7,6 +8,7 @@ import '../../../components/code/divider_code.dart';
 import '../../../components/code/sized_space_code.dart';
 import '../../../components/code/text/googleFonts.dart';
 import '../../../components/code/webImageInput_code.dart';
+import '../../../models/api/category_api_models.dart';
 
 class SubCategorySideScreen extends StatefulWidget {
   static const String routeName = '/subCategoryScreen';
@@ -18,9 +20,12 @@ class SubCategorySideScreen extends StatefulWidget {
 }
 
 class _SubCategorySideScreenState extends State<SubCategorySideScreen> {
+  final SubCategoryControllers subCategoryController = SubCategoryControllers();
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  late String categoryName;
+  late String subCategoryName;
+
+  CategoryApiModels? selectedCategory;
 
   dynamic _categoryImage;
 
@@ -56,152 +61,185 @@ class _SubCategorySideScreenState extends State<SubCategorySideScreen> {
   Widget build(BuildContext context) {
     double mediaQueryWidth = MediaQuery.of(context).size.width;
 
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 4,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: googleText(
-                  'Subcategory',
-                  fontSize: 36,
-                ),
+    return Stack(
+      children: [
+        Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
               ),
-            ),
-            divider(),
-            SubCategoryWidget(),
-            sizedBoxMediaQuery(
-              context,
-              width: 0,
-              height: 0.01,
-            ),
-            Row(
-              children: [
-                webImageInput(
-                  _categoryImage,
-                  'Category Image',
-                  context,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: mediaQueryWidth * 0.15,
-                    child: TextFormField(
-                      onChanged: (value) {
-                        categoryName = value;
-                      },
-                      validator: (value) {
-                        if (value == null ||
-                            value.isEmpty ||
-                            value.length <= 3) {
-                          return 'Please Enter Valid Category Name';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        labelText: 'Enter Category Name',
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: googleText(
+                        'Category',
+                        fontSize: 36,
                       ),
                     ),
                   ),
-                ),
-                sizedBoxMediaQuery(
-                  context,
-                  width: 0.023,
-                  height: 0,
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: webButtonGoogleText(
-                    'Cancel',
+                  divider(),
+                  SubCategoryWidget(),
+                  sizedBoxMediaQuery(
+                    context,
+                    width: 0,
+                    height: 0.01,
                   ),
-                ),
-                elevatedButton(
-                  () async {
-                    setState(() {
-                      _isLoading = true;
-                    });
-                    if (_categoryImage == null) {
-                      if (!_isSnackBarVisible) {
-                        _isSnackBarVisible = true;
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(
-                              SnackBar(
-                                content: Text('Please add an image'),
-                                duration: Duration(seconds: 2),
-                              ),
-                            )
-                            .closed
-                            .then((_) {
-                          _isSnackBarVisible = false;
-                        });
-                      }
-                      setState(() {
-                        _isLoading = false;
-                      });
-                      return;
-                    }
-                    if (_formKey.currentState!.validate()) {
-                      // dynamic newImage = await simulateImageUpload();
-                      // setState(() {
-                      //   _categoryImage = newImage;
-                      //   _bannerImage = newImage;
-                      //   _isLoading = false;
-                      // });
+                  Row(
+                    children: [
+                      webImageInput(
+                        _categoryImage,
+                        'Category Image',
+                        context,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          width: mediaQueryWidth * 0.15,
+                          child: TextFormField(
+                            onChanged: (value) {
+                              subCategoryName = value;
+                            },
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  value.length <= 3) {
+                                return 'Please Enter Valid Category Name';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Enter Category Name',
+                            ),
+                          ),
+                        ),
+                      ),
+                      sizedBoxMediaQuery(
+                        context,
+                        width: 0.023,
+                        height: 0,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: webButtonGoogleText(
+                          'Cancel',
+                        ),
+                      ),
+                      elevatedButton(
+                        () async {
+                          setState(() {
+                            _isLoading = true;
+                          });
 
-                      reloadWidget(); // Only called when validation passes
-                    } else {
-                      setState(() {
-                        _isLoading = false; // Reset loading if validation fails
-                      });
-                    }
-                  },
-                  "Submit",
+                          if (_categoryImage == null) {
+                            if (!_isSnackBarVisible) {
+                              _isSnackBarVisible = true;
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(
+                                    SnackBar(
+                                      content: Text('Please add an image'),
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  )
+                                  .closed
+                                  .then((_) {
+                                _isSnackBarVisible = false;
+                              });
+                            }
+                            setState(() {
+                              _isLoading = false;
+                            });
+                            return;
+                          }
+
+                          if (selectedCategory == null) {
+                            if (!_isSnackBarVisible) {
+                              _isSnackBarVisible = true;
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(
+                                    SnackBar(
+                                      content: Text('Please select a category'),
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  )
+                                  .closed
+                                  .then((_) {
+                                _isSnackBarVisible = false;
+                              });
+                            }
+                            setState(() {
+                              _isLoading = false;
+                            });
+                            return;
+                          }
+
+                          if (_formKey.currentState!.validate()) {
+                            await subCategoryController.uploadSubCategory(
+                              categoryId: selectedCategory!.categoryId,
+                              categoryName: selectedCategory!.categoryName,
+                              subCategoryName: subCategoryName,
+                              subCategoryPickedImage: _categoryImage,
+                              context: context,
+                            );
+
+                            setState(() {
+                              _categoryImage = null;
+                              _isLoading = false;
+                            });
+                          } else {
+                            setState(() {
+                              _isLoading = false;
+                            });
+                          }
+                        },
+                        "Submit",
+                      ),
+                    ],
+                  ),
+                  sizedBoxMediaQuery(
+                    context,
+                    width: 0,
+                    height: 0.02,
+                  ),
+                  elevatedButton(
+                    () {
+                      uploadImage(
+                        (img) {
+                          setState(() {
+                            _categoryImage = img;
+                          });
+                        },
+                      );
+                    },
+                    "Upload Image",
+                  ),
+                  divider(),
+                ],
+              ),
+            ),
+          ),
+        ),
+        if (_isLoading)
+          Positioned.fill(
+            child: Stack(
+              children: [
+                ModalBarrier(
+                  dismissible: false,
+                  color: Colors.black.withValues(alpha: .5),
+                ),
+                Center(
+                  child: CircularProgressIndicator(),
                 ),
               ],
             ),
-            sizedBoxMediaQuery(
-              context,
-              width: 0,
-              height: 0.02,
-            ),
-            elevatedButton(
-              () {
-                uploadImage(
-                  (img) {
-                    setState(() {
-                      _categoryImage = img;
-                    });
-                  },
-                );
-              },
-              "Upload Image",
-            ),
-            divider(),
-            if (_isLoading)
-              Positioned.fill(
-                child: Stack(
-                  children: [
-                    ModalBarrier(
-                      dismissible: false,
-                      color: Colors.black.withValues(alpha: .5),
-                    ),
-                    Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  ],
-                ),
-              ),
-          ],
-        ),
-      ),
+          ),
+      ],
     );
   }
 }
