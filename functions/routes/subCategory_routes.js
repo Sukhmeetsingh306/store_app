712e310d8cf2b7d15ee0865dcf922e58c1158cb5
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-21 18:27:49 +0530

    code for getting the services on the code

--- functions/routes/subCategory_routes.js
+++ functions/routes/subCategory_routes.js
@@ -3,23 +3,36 @@ import { SubCategory } from "../models/subCategory_models.js";
 
 const subCategoryRouter = express.Router();
 
-subCategoryRouter.route("/api/subCategory").post(async (req, res) => {
-  try {
-    const { categoryId, categoryName, subCategoryImage, subCategoryName } = req.body;
-    const newSubCategory = new SubCategory({
-      categoryId,
-      categoryName,
-      subCategoryImage,
-      subCategoryName,
-    });
-    await newSubCategory.save();
-    console.log("SubCategory", newSubCategory, "saved successfully");
-    return res.status(201).json(newSubCategory);
-  } catch (e) {
-    console.log("Error in creating SubCategory", e);
-    return res.status(400).json({ error: e.message });
-  }
-});
+subCategoryRouter
+  .route("/api/subCategory")
+  .post(async (req, res) => {
+    try {
+      const { categoryId, categoryName, subCategoryImage, subCategoryName } =
+        req.body;
+      const newSubCategory = new SubCategory({
+        categoryId,
+        categoryName,
+        subCategoryImage,
+        subCategoryName,
+      });
+      await newSubCategory.save();
+      console.log("SubCategory", newSubCategory, "saved successfully");
+      return res.status(201).json(newSubCategory);
+    } catch (e) {
+      console.log("Error in creating SubCategory", e);
+      return res.status(400).json({ error: e.message });
+    }
+  })
+  .get(async (req, res) => {
+    try {
+      const subCategories = await SubCategory.find();
+      console.log("Subcategories", subCategories, "success in get");
+      return res.status(200).json(subCategories);
+    } catch (e) {
+      console.log("Error in getting subcategories", e);
+      return res.status(400).json({ error: e.message });
+    }
+  });
 
 // Create a this get request here as this is working on the same category as the adding the category
 subCategoryRouter
