23ec3c8fbe38174e1832a2fbc19f9c96f02ca32f
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-23 19:36:25 +0530

    solved all the warning

--- lib/controllers/subCategory_controllers.dart
+++ lib/controllers/subCategory_controllers.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'dart:convert';
 
 import 'package:cloudinary_public/cloudinary_public.dart';
--- lib/views/screens/navigation_web/subCategory_side_screen.dart
+++ lib/views/screens/navigation_web/subCategory_side_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:store_app/controllers/subCategory_controllers.dart';
