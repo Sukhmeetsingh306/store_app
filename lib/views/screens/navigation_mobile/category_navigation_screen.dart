566c96d9b3bfb4daa381578891ca0dbbb823f0b3
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-01-25 00:05:12 +0530

    convert the state for the statefull

--- lib/views/screens/navigation_mobile/category_navigation_screen.dart
+++ lib/views/screens/navigation_mobile/category_navigation_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 
-class CategoryNavigationScreen extends StatelessWidget {
+import 'widgets/header_widget_screen.dart';
+
+class CategoryNavigationScreen extends StatefulWidget {
   const CategoryNavigationScreen({super.key});
 
   @override
+  State<CategoryNavigationScreen> createState() => _CategoryNavigationScreenState();
+}
+
+class _CategoryNavigationScreenState extends State<CategoryNavigationScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(child: Text("cart")),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          MediaQuery.of(context).size.height * 20,
+        ),
+        child: HeaderWidgetScreen(),
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.grey.shade300,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
