23842994e64267eb2d0b46703446f51ab8920dec
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-03-31 00:59:25 +0530

    created the dummy page

---
+++ multi_vendor/lib/screen/authentication/login_auth_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginAuthScreen extends StatefulWidget {
+  const LoginAuthScreen({super.key});
+
+  static String routeName = 'loginPage';
+  static String routePath = '/loginPage';
+
+  @override
+  State<LoginAuthScreen> createState() => _LoginAuthScreenState();
+}
+
+class _LoginAuthScreenState extends State<LoginAuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
