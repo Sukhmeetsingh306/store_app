f31f734885d2ae869495aee5f71cd22182053583
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-04-02 00:12:46 +0530

    this is to make sure if any of the code goes wrong for the image it can fill it

--- multi_vendor/lib/controllers/login_user_controllers.dart
+++ multi_vendor/lib/controllers/login_user_controllers.dart
@@ -1,24 +1,33 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:random_avatar/random_avatar.dart'; // Add this import
 import 'package:multi_vendor/globals_variables.dart';
 import 'package:multi_vendor/models/login_user_models.dart';
 import 'package:multi_vendor/services/http_services.dart';
 
 class LoginUserControllers {
   Future<void> signUpUsers({
-    required context,
+    required BuildContext context,
     required String email,
     required String name,
     required String password,
+    required String phone,
+    required int age,
+    String? image, // Optional
     bool? isSeller,
   }) async {
     try {
+      // Generate random avatar if image is null
+      image = image ?? generateRandomAvatar();
+
       LoginUserModel user = LoginUserModel(
         id: "",
         name: name,
         email: email,
+        phone: phone,
+        age: age,
+        image: image, // Default avatar or custom image
         state: "",
         city: "",
         locality: "",
@@ -28,8 +37,8 @@ class LoginUserControllers {
       );
 
       http.Response response = await http.post(
-        Uri.parse('$webUri/signup/'),
-        body: user.toJson(),
+        Uri.parse('$webUri/signup'),
+        body: jsonEncode(user.toJson()),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
@@ -41,23 +50,30 @@ class LoginUserControllers {
         response: response,
         context: context,
         onSuccess: () {
-          //showSnackBar(context, 'Account has been Created');
-          print("Account is successfully Created");
+          print("Account successfully created!");
         },
       );
     } catch (e) {
-      // showSnackBar(context, 'Error: ${e.toString()}');
       print('Error: ${e.toString()}');
     }
   }
 
+  // Function to generate a random avatar using RandomAvatar package
+  String generateRandomAvatar() {
+    // Returns a unique random avatar URL using the current time
+    return RandomAvatar(
+      DateTime.now().toIso8601String(), // Unique seed for avatar
+      height: 90,
+      width: 90,
+    ).toString();
+  }
+
   Future<bool> signInUsers({
     required BuildContext context,
-    String? email,
-    String? password,
+    required String email,
+    required String password,
   }) async {
     try {
-      // Sending the HTTP POST request
       http.Response response = await http.post(
         Uri.parse("$webUri/signin"),
         body: jsonEncode({
@@ -70,16 +86,12 @@ class LoginUserControllers {
       ).timeout(const Duration(seconds: 10), onTimeout: () {
         throw Exception('Request timed out');
       });
+
       final responseData = jsonDecode(response.body);
 
       if (response.statusCode == 200) {
-        var responseData = jsonDecode(response.body);
         String token = responseData['token'];
-        // You can store the token or navigate the user to another screen
         print("Login successful: Token: $token");
-        print("User is validated!");
-        print("Email: $email");
-        print("Password: $password");
         return true;
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -89,11 +101,8 @@ class LoginUserControllers {
         return false;
       }
     } catch (e) {
-      // Handle any exceptions thrown during the request
       print('Error: ${e.toString()}');
       return false;
-
-      // Show AlertDialog with a general error message
     }
   }
 }
--- multi_vendor/lib/models/login_user_models.dart
+++ multi_vendor/lib/models/login_user_models.dart
@@ -1,39 +1,58 @@
 import 'dart:convert';
+import 'package:random_avatar/random_avatar.dart';
 
 class LoginUserModel {
   final String id;
   final String name;
   final String email;
+  final String phone;
+  final int age;
   final String state;
   final String city;
   final String locality;
   final String password;
   final String token;
   final bool isSeller;
+  final String image; // Ensured to always have a value
 
   LoginUserModel({
     required this.id,
     required this.name,
     required this.email,
+    required this.phone,
+    required this.age,
     required this.state,
     required this.city,
     required this.locality,
     required this.password,
     required this.token,
     required this.isSeller,
-  });
+    String? image,
+  }) : image = image ?? generateRandomAvatar(); // Use random avatar if null
+
+  // Generates a random avatar using the package
+  static String generateRandomAvatar() {
+    return RandomAvatar(
+      DateTime.now().toIso8601String(), // Unique seed for avatar
+      height: 90,
+      width: 90,
+    ).toString();
+  }
 
   Map<String, dynamic> toUser() {
     return {
       '_id': id,
       'name': name,
       'email': email,
+      'phone': phone,
+      'age': age,
       'state': state,
       'city': city,
       'locality': locality,
       'password': password,
       'token': token,
       'isSeller': isSeller,
+      'image': image, // Always has a value
     };
   }
 
@@ -43,12 +62,15 @@ class LoginUserModel {
       id: map['_id'] ?? "",
       name: map['name'] ?? "",
       email: map['email'] ?? "",
+      phone: map['phone'] ?? "",
+      age: map['age'] ?? 18, // Default to 18 if missing
       state: map['state'] ?? "",
       city: map['city'] ?? "",
       locality: map['locality'] ?? "",
       password: map['password'] ?? "",
       token: map['token'] ?? "",
       isSeller: map['isSeller'] ?? false,
+      image: map['image'],
     );
   }
 
