51022d1f028147d53f90101c7f08768cd24b3aef
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-04-01 23:59:29 +0530

    added new fields

--- multi_vendor/functions/models/userInf.js
+++ multi_vendor/functions/models/userInf.js
@@ -22,44 +22,44 @@ const userSchema = new mongoose.Schema(
         message: "Please enter a valid email address.",
       },
     },
-
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
     state: {
       type: String,
       default: "",
     },
-
     city: {
       type: String,
       default: "",
     },
-
     locality: {
       type: String,
       default: "",
     },
-
     password: {
       type: String,
       required: [true, "Password is required."],
       validate: {
         validator: (value) => {
-          // Check minimum length
           if (value.length < 8) {
             throw new Error("Password must be at least 8 characters long.");
           }
-
-          // Regular expression to check for required complexity
-          // const passwordRegex = /r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$'/;
-          // if (!passwordRegex.test(value)) {
-          //   throw new Error('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
-          // }
-
-          return true; // If all checks pass, the validation succeeds
+          return true;
         },
         message: "Invalid password.",
       },
     },
-
     isSeller: {
       type: Boolean,
       default: false,
