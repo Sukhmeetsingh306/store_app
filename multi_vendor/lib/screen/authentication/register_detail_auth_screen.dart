34e3e6b4c48433e67c45c383e4508ea78c91908a
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-04-05 14:09:11 +0530

    code distributed

--- multi_vendor/lib/screen/authentication/register_detail_auth_screen.dart
+++ multi_vendor/lib/screen/authentication/register_detail_auth_screen.dart
@@ -127,6 +127,157 @@ class _RegisterDetailAuthScreenState extends State<RegisterDetailAuthScreen> {
     );
   }
 
+  Widget pageInnerCode(BuildContext context, bool isLargeScreen) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        googleInterText(
+          'Create Account',
+          fontWeight: FontWeight.w700,
+          fontSize: 24,
+        ),
+        sizedBoxH8(),
+        googleInterText(
+          'Sign up now and start exploring all that our\napp has to offer. We\'re excited to welcome\nyou to our community!',
+          fontWeight: FontWeight.w400,
+          fontSize: 14,
+        ),
+        sizedBoxH10(),
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              // Profile Image Upload Section
+              Center(
+                child: CircleAvatar(
+                  backgroundColor: ColorTheme.color.whiteColor,
+                  radius: 45,
+                  backgroundImage: kIsWeb
+                      ? (_webImage != null ? MemoryImage(_webImage!) : null)
+                      : (_imageFile != null ? FileImage(_imageFile!) : null),
+                  child: GestureDetector(
+                    onTap: _pickImage,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white24,
+                            blurRadius: 10,
+                            spreadRadius: 2,
+                            offset: Offset(4, 4),
+                          ),
+                        ],
+                      ),
+                      child: (_imageFile == null && _webImage == null)
+                          ? const Icon(
+                              Icons.person_add_alt_1_outlined,
+                              size: 50,
+                              color: Colors.grey,
+                            )
+                          : null,
+                    ),
+                  ),
+                ),
+              ),
+              sizedBoxH8(),
+              googleInterText(
+                'Upload a photo for us to easily identify you.',
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+              sizedBoxH15(),
+              textFormField(
+                _nameController,
+                'Your Name',
+                (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              sizedBoxH15(),
+              textFormField(
+                _ageController,
+                'Your Age',
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your age';
+                  }
+                  return null;
+                },
+              ),
+              sizedBoxH15(),
+              IntlPhoneField(
+                controller: _phoneController,
+                decoration: InputDecoration(
+                  labelText: 'Phone Number',
+                  labelStyle: GoogleFonts.getFont(
+                    'Inter',
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                  hintText: 'Your Number',
+                  hintStyle: GoogleFonts.getFont(
+                    'Inter',
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.transparent,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                ),
+                keyboardType: TextInputType.phone,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                initialCountryCode: 'US',
+                dropdownTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+                dropdownIcon: Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.black,
+                ),
+                onChanged: (phone) {},
+                validator: (phone) {
+                  if (phone == null || phone.number.isEmpty) {
+                    return 'Please enter your phone number';
+                  }
+                  return null;
+                },
+                style: GoogleFonts.getFont(
+                  'Inter',
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+              ),
+              sizedBoxH15(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget pageCode(BuildContext context, bool isLargeScreen) {
     return ConstrainedBox(
       constraints: BoxConstraints(
@@ -150,211 +301,59 @@ class _RegisterDetailAuthScreenState extends State<RegisterDetailAuthScreen> {
           children: [
             Expanded(
               child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    googleInterText(
-                      'Create Account',
-                      fontWeight: FontWeight.w700,
-                      fontSize: 24,
-                    ),
-                    sizedBoxH8(),
-                    googleInterText(
-                      'Sign up now and start exploring all that our\napp has to offer. We\'re excited to welcome\nyou to our community!',
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14,
-                    ),
-                    sizedBoxH10(),
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          // Profile Image Upload Section
-                          Center(
-                            child: CircleAvatar(
-                              backgroundColor: ColorTheme.color.whiteColor,
-                              radius: 45,
-                              backgroundImage: kIsWeb
-                                  ? (_webImage != null
-                                      ? MemoryImage(_webImage!)
-                                      : null)
-                                  : (_imageFile != null
-                                      ? FileImage(_imageFile!)
-                                      : null),
-                              child: GestureDetector(
-                                onTap: _pickImage,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.white24,
-                                        blurRadius: 10,
-                                        spreadRadius: 2,
-                                        offset: Offset(4, 4),
-                                      ),
-                                    ],
-                                  ),
-                                  child:
-                                      (_imageFile == null && _webImage == null)
-                                          ? const Icon(
-                                              Icons.person_add_alt_1_outlined,
-                                              size: 50,
-                                              color: Colors.grey,
-                                            )
-                                          : null,
-                                ),
-                              ),
-                            ),
-                          ),
-                          sizedBoxH8(),
-                          googleInterText(
-                            'Upload a photo for us to easily identify you.',
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                          ),
-                          sizedBoxH15(),
-                          textFormField(
-                            _nameController,
-                            'Your Name',
-                            (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter your name';
-                              }
-                              return null;
-                            },
-                          ),
-                          sizedBoxH15(),
-                          textFormField(
-                            _ageController,
-                            'Your Age',
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                            (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter your age';
-                              }
-                              return null;
-                            },
-                          ),
-                          sizedBoxH15(),
-                          IntlPhoneField(
-                            controller: _phoneController,
-                            decoration: InputDecoration(
-                              labelText: 'Phone Number',
-                              labelStyle: GoogleFonts.getFont(
-                                'Inter',
-                                color: Colors.black,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16,
-                              ),
-                              hintText: 'Your Number',
-                              hintStyle: GoogleFonts.getFont(
-                                'Inter',
-                                color: Colors.black,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16,
-                              ),
-                              border: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey),
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey),
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey),
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              filled: true,
-                              fillColor: Colors.transparent,
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 20, horizontal: 16),
-                            ),
-                            keyboardType: TextInputType.phone,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                            initialCountryCode: 'US',
-                            dropdownTextStyle: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                            ),
-                            dropdownIcon: Icon(
-                              Icons.arrow_drop_down,
-                              color: Colors.black,
-                            ),
-                            onChanged: (phone) {},
-                            validator: (phone) {
-                              if (phone == null || phone.number.isEmpty) {
-                                return 'Please enter your phone number';
-                              }
-                              return null;
-                            },
-                            style: GoogleFonts.getFont(
-                              'Inter',
-                              color: Colors.black,
-                              fontWeight: FontWeight.w400,
-                              fontSize: 16,
-                            ),
-                          ),
-                          sizedBoxH15(),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+                  child: pageInnerCode(context, isLargeScreen)),
             ),
             // Place the button at the bottom
-            Column(
-              children: [
-                Center(child: TermsAndConditionsText()),
-                sizedBoxH15(),
-                AppTextButton(
-                  buttonText: 'Create Account',
-                  onPressed: () async {
-                    // Handle create account logic
-                  },
-                ),
-                sizedBoxH10(),
-                InkWell(
-                  onTap: () async {
-                    if (Navigator.of(context).canPop()) {
-                      pop(context);
-                    } else {
-                      pushNamedAndRemoveUntil(context, '/registerPage');
-                    }
-                  },
-                  child: Center(
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        children: [
-                          textSpan(
-                            'Preview Details?',
-                            fontSize: 14,
-                          ),
-                          textSpan(
-                            ' Preview',
-                            fontSize: 14,
-                            color: const Color.fromRGBO(36, 124, 255, 1),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                sizedBoxH5(),
-                Divider(),
-              ],
-            ),
+            buttonBottomCodeDetail(_formKey, context),
+            Divider(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget buttonBottomCodeDetail(
+      GlobalKey<FormState> formKey, BuildContext context) {
+    return Column(
+      children: [
+        Center(child: TermsAndConditionsText()),
+        sizedBoxH15(),
+        AppTextButton(
+          buttonText: 'Create Account',
+          onPressed: () async {
+            // Handle create account logic
+          },
+        ),
+        sizedBoxH10(),
+        InkWell(
+          onTap: () async {
+            if (Navigator.of(context).canPop()) {
+              pop(context);
+            } else {
+              pushNamedAndRemoveUntil(context, '/registerPage');
+            }
+          },
+          child: Center(
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: [
+                  textSpan(
+                    'Preview Details?',
+                    fontSize: 14,
+                  ),
+                  textSpan(
+                    ' Preview',
+                    fontSize: 14,
+                    color: const Color.fromRGBO(36, 124, 255, 1),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        sizedBoxH5(),
+      ],
     );
   }
 }
