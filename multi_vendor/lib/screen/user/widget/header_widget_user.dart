cb68874ce26233c874054bd9857509e5c8c0a63c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-04-18 18:23:11 +0530

    code adjusted for the mobile

--- multi_vendor/lib/screen/user/widget/header_widget_user.dart
+++ multi_vendor/lib/screen/user/widget/header_widget_user.dart
@@ -10,7 +10,7 @@ class HeaderWidgetUser extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return sizedBoxHeaderData(context);
+    return sizedBoxHeaderData(context, arrowPresent: false);
   }
 }
 
@@ -69,116 +69,23 @@ PreferredSizeWidget detailHeaderWidget(
   BuildContext context, {
   VoidCallback? backOnPressed,
 }) {
-  double mediaQueryWidth = MediaQuery.of(context).size.width;
   double mediaQueryHeight = MediaQuery.of(context).size.height;
 
   return PreferredSize(
     preferredSize: Size.fromHeight(mediaQueryHeight * 0.17),
-    child: SizedBox(
-      width: mediaQueryWidth,
-      height: mediaQueryHeight * 0.17,
-      child: Stack(
-        children: [
-          Image.asset(
-            'assets/images/arrow.png',
-            width: mediaQueryWidth,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            left: mediaQueryWidth * 0.02,
-            top: mediaQueryHeight * 0.1,
-            child: IconButton(
-              onPressed: backOnPressed,
-              icon: Icon(
-                Icons.arrow_back_ios_new,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: mediaQueryHeight * 0.013,
-            left: mediaQueryWidth * 0.15,
-            right: mediaQueryWidth * 0.23,
-            child: SizedBox(
-              width: 250,
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter",
-                  hintStyle: TextStyle(
-                    fontSize: 14,
-                    color: Color(0xFF7F7F7F),
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(
-                    vertical: 16,
-                    horizontal: 12,
-                  ),
-                  prefixIcon: Icon(
-                    Icons.search_rounded,
-                  ),
-                  suffixIcon: Icon(
-                    Icons.camera_alt_outlined,
-                  ),
-                  fillColor: Colors.grey.shade200,
-                  filled: true,
-                  focusColor: ColorTheme.color.blackColor,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: mediaQueryHeight * 0.022,
-            right: mediaQueryWidth * 0.12,
-            child: Material(
-              type: MaterialType.transparency,
-              child: InkWell(
-                onTap: () {},
-                child: Ink(
-                  width: 35,
-                  height: 35,
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    Icons.notifications_active_outlined,
-                    size: 28,
-                    color: ColorTheme.color.whiteColor,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: mediaQueryHeight * 0.022,
-            right: mediaQueryWidth * 0.02,
-            child: Material(
-              type: MaterialType.transparency,
-              child: InkWell(
-                onTap: () {},
-                child: Ink(
-                  width: 35,
-                  height: 35,
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    Icons.messenger_outline_outlined,
-                    size: 28,
-                    color: ColorTheme.color.whiteColor,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+    child: sizedBoxHeaderData(
+      context,
+      arrowPresent: true,
+      backOnPressed: backOnPressed,
     ),
   );
 }
 
-Widget sizedBoxHeaderData(BuildContext context) {
+Widget sizedBoxHeaderData(
+  BuildContext context, {
+  bool arrowPresent = false,
+  VoidCallback? backOnPressed,
+}) {
   double mediaQueryWidth = MediaQuery.of(context).size.width;
   double mediaQueryHeight = MediaQuery.of(context).size.height;
 
@@ -256,6 +163,18 @@ Widget sizedBoxHeaderData(BuildContext context) {
                     ),
                   ),
                 ),
+                if (arrowPresent)
+                  Positioned(
+                    left: mediaQueryWidth * 0.02,
+                    top: mediaQueryHeight * 0.1,
+                    child: IconButton(
+                      onPressed: backOnPressed,
+                      icon: Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 Flexible(
                   flex: getResponsiveFlex(mediaQueryWidth),
                   child: Padding(
@@ -318,41 +237,55 @@ Widget sizedBoxHeaderData(BuildContext context) {
             ),
           ),
         if (!isWebMobile)
-          Positioned(
-            bottom: mediaQueryHeight * 0.013,
-            left: mediaQueryWidth * (isWebMobile ? 0.20 : 0.08),
-            right: mediaQueryWidth * (isWebMobile ? 0.30 : 0.23),
-            child: SizedBox(
-              width: 250,
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter",
-                  hintStyle: TextStyle(
-                    fontSize: 14,
-                    color: Color(0xFF7F7F7F),
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(
-                    vertical: 16,
-                    horizontal: 12,
-                  ),
-                  prefixIcon: Icon(
-                    Icons.search_rounded,
-                  ),
-                  suffixIcon: Icon(
-                    Icons.camera_alt_outlined,
-                  ),
-                  fillColor: Colors.grey.shade200,
-                  filled: true,
-                  focusColor: ColorTheme.color.blackColor,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none,
-                  ),
+          if (arrowPresent)
+            Positioned(
+              top: mediaQueryHeight * 0.1,
+              child: IconButton(
+                onPressed: backOnPressed,
+                icon: Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+        Positioned(
+          bottom: mediaQueryHeight * 0.013,
+          left: mediaQueryWidth *
+              (isWebMobile
+                  ? (arrowPresent ? 0.25 : 0.20)
+                  : (arrowPresent ? 0.12 : 0.08)),
+          right: mediaQueryWidth * (isWebMobile ? 0.30 : 0.23),
+          child: SizedBox(
+            width: 250,
+            height: 50,
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Enter",
+                hintStyle: TextStyle(
+                  fontSize: 14,
+                  color: Color(0xFF7F7F7F),
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 12,
+                ),
+                prefixIcon: Icon(
+                  Icons.search_rounded,
+                ),
+                suffixIcon: Icon(
+                  Icons.camera_alt_outlined,
+                ),
+                fillColor: Colors.grey.shade200,
+                filled: true,
+                focusColor: ColorTheme.color.blackColor,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide.none,
                 ),
               ),
             ),
           ),
+        ),
         if (!isWebMobile)
           Positioned(
             bottom: mediaQueryHeight * 0.022,
