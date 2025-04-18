e191c53b8806e833b3493fccc4e9e9264ff3b6de
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-04-18 16:46:39 +0530

    new code added for the showing the furthur category screen

---
+++ multi_vendor/lib/screen/user/widget/inner_category_widget_user.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/api/category_api_models.dart';
+import '../../../utils/routes/navigation_routes.dart';
+import '../../../utils/theme/color/color_theme.dart';
+import '../../../utils/widget/button_widget_utils.dart';
+import 'header_widget_user.dart';
+import 'navigation/account_navigation_screen.dart';
+import 'navigation/cart_navigation_screen.dart';
+import 'navigation/category_navigation_screen.dart';
+import 'navigation/fav_navigation_screen.dart';
+import 'navigation/store_navigation_screen.dart';
+import 'support/inner_category_content_support_widget_user.dart';
+
+class InnerCategoryScreen extends StatefulWidget {
+  final CategoryApiModels category;
+  const InnerCategoryScreen({super.key, required this.category});
+
+  @override
+  State<InnerCategoryScreen> createState() => _InnerCategoryScreenState();
+}
+
+class _InnerCategoryScreenState extends State<InnerCategoryScreen> {
+  int mobilePagesIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> mobilePages = [
+      InnerCategoryContentSupportWidgetUser(
+        category: widget.category,
+      ),
+      FavNavigationScreen(),
+      CategoryNavigationScreen(
+        hasAppBar: false,
+      ),
+      StoreNavigationScreen(),
+      CartNavigationScreen(),
+      AccountNavigationScreen(),
+    ];
+    return Scaffold(
+      appBar: detailHeaderWidget(
+        context,
+        backOnPressed: () => pop(context),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: mobilePagesIndex,
+        selectedItemColor: ColorTheme.color.deepPuceColor,
+        selectedFontSize: 14,
+        unselectedFontSize: 12,
+        unselectedItemColor: ColorTheme.color.grayColor,
+        onTap: (value) => setState(
+          () {
+            mobilePagesIndex = value;
+          },
+        ),
+        items: [
+          bottomBarItem(
+            Icon(Icons.home_outlined),
+            'Home',
+          ),
+          bottomBarItem(
+            Icon(Icons.favorite_border_outlined),
+            'Favorite',
+          ),
+          bottomBarItem(
+            Icon(Icons.category_outlined),
+            'Categories',
+          ),
+          bottomBarItem(
+            Icon(Icons.shopping_bag_outlined),
+            'Stores',
+          ),
+          bottomBarItem(
+            Icon(Icons.shopping_cart_outlined),
+            'Cart',
+          ),
+          bottomBarItem(
+            Icon(Icons.account_circle_outlined),
+            'Account',
+          ),
+        ],
+        elevation: 5,
+      ),
+      body: mobilePages[mobilePagesIndex],
+    );
+  }
+}
