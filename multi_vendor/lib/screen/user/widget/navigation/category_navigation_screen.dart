ab8ae47228041d9c1dd3abc95822210cdbddc012
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-04-25 14:54:49 +0530

    fixed the import issue

--- multi_vendor/lib/screen/user/widget/navigation/category_navigation_screen.dart
+++ multi_vendor/lib/screen/user/widget/navigation/category_navigation_screen.dart
@@ -5,19 +5,27 @@ import '../support/category_widget_support_user.dart';
 
 class CategoryNavigationScreen extends StatelessWidget {
   final bool hasAppBar;
+  final GlobalKey<ScaffoldState> scaffoldKey;
 
-  const CategoryNavigationScreen({super.key, bool? hasAppBar})
-      : hasAppBar = hasAppBar ?? true; // Ensures hasAppBar is never null
+  CategoryNavigationScreen({
+    super.key,
+    bool? hasAppBar,
+    GlobalKey<ScaffoldState>? scaffoldKey,
+  })  : hasAppBar = hasAppBar ?? true,
+        scaffoldKey = scaffoldKey ?? GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: hasAppBar
           ? PreferredSize(
               preferredSize: Size.fromHeight(
                 MediaQuery.of(context).size.height * 0.2,
               ),
-              child: HeaderWidgetUser(),
+              child: HeaderWidgetUser(
+                scaffoldKey: scaffoldKey,
+              ),
             )
           : null,
       body: Row(
