ba1df9dde0f58809166921f3ce14f91538404a4f
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-06-06 21:44:35 +0530

    altered the code by storing data in sharedpreference and navigating back to the main screen of the home after storing the data

--- multi_vendor/lib/controllers/login_user_controllers.dart
+++ multi_vendor/lib/controllers/login_user_controllers.dart
@@ -1,10 +1,17 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'package:random_avatar/random_avatar.dart'; // Add this import
 import 'package:multi_vendor/globals_variables.dart';
 import 'package:multi_vendor/models/login_user_models.dart';
 import 'package:multi_vendor/services/http_services.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../provider/user_provider.dart';
+
+final riverpodContainer = ProviderContainer();
 
 class LoginUserControllers {
   Future<void> signUpUsers({
@@ -88,10 +95,37 @@ class LoginUserControllers {
       });
 
       final responseData = jsonDecode(response.body);
+      // or use json.decode(response.body)
 
       if (response.statusCode == 200) {
-        String token = responseData['token'];
-        print("Login successful: Token: $token");
+        // String token = responseData['token'];
+        // print("Login successful: Token: $token");
+        // return true;
+
+        // accessing sharedPreferences for token and user data storage
+        SharedPreferences pref = await SharedPreferences.getInstance();
+        String token =
+            responseData['token']; // extracting token from response body
+
+        //storing token and user data in sharedPreferences
+        await pref.setString('auth_token', token);
+
+        // encode user data received from the backend
+        final userJson = jsonEncode(responseData['user']);
+
+        //update the application state with riverpod
+        riverpodContainer.read(userProvider.notifier).setUser(userJson);
+
+        //storing data in the user provider
+        await pref.setString('user', userJson);
+
+        if (Navigator.of(context).canPop()) {
+          Navigator.of(context).popUntil((route) => route.isFirst);
+          context.pushReplacement('/homePage');
+        } else {
+          context.go('/homePage');
+        }
+
         return true;
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
