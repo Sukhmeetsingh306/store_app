510640c47458f63cd1e6542642c773ce959238a4
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-06-06 23:41:09 +0530

    added the provider code as per to check for the token

--- multi_vendor/lib/main.dart
+++ multi_vendor/lib/main.dart
@@ -6,11 +6,11 @@ void main() {
   runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       title: 'Multi Vendor',
       routerConfig: AppRoutes.router,
