a425d1ff164199366a8b05b0bb5c30841ff11f8c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-08-26 15:10:07 +0530

    fix: update user authentication flow to parse user JSON and navigate based on roles

--- multi_vendor/lib/utils/routes/splash_screen_route.dart
+++ multi_vendor/lib/utils/routes/splash_screen_route.dart
@@ -4,6 +4,8 @@ import 'package:go_router/go_router.dart';
 import 'package:multi_vendor/provider/user_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../models/login_user_models.dart';
+
 class SplashScreenRoute extends ConsumerStatefulWidget {
   const SplashScreenRoute({super.key});
 
@@ -19,20 +21,35 @@ class _SplashScreenRouteState extends ConsumerState<SplashScreenRoute> {
   }
 
   Future<void> _checkAuthTokenAndNavigate() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    String? token = pref.getString('auth_token');
-    String? userJson = pref.getString('user');
+    final pref = await SharedPreferences.getInstance();
+    final token = pref.getString('auth_token');
+    final userJson = pref.getString('user');
 
     if (!mounted) return;
+
     if (token != null && userJson != null) {
-      ref.read(userProvider.notifier).setUser(userJson);
-      context.go('/homePage');
+      // Parse saved user JSON into model
+      final user = LoginUserModel.fromJson(userJson);
+
+      // Save in provider
+      ref.read(userProvider.notifier).updateUser(user);
+
+      // Navigate based on role
+      if (user.roles.contains("seller")) {
+        context.go('/management'); // Seller dashboard
+      } else if (user.roles.contains("admin")) {
+        context.go('/adminDashboard'); // Future use
+      } else {
+        context.go('/homePage'); // Default consumer dashboard
+      }
     } else {
+      // No token/user → logout
       ref.read(userProvider.notifier).signOut();
       if (context.mounted) {
         context.go('/loginPage');
       }
     }
+
     // MARK: check for the signout way that if the user is had entered check for the cookie if already entered check for the data else navigate to login page else stay in the home page
   }
 
