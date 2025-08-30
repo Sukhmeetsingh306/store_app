30dfc2e9cd6480b62d34d96412eaff27b51c0120
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-08-30 21:03:12 +0530

    feat: add MobileSellerScreen component for seller dashboard interface

---
+++ multi_vendor/lib/screen/seller/mobile_seller_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MobileSellerScreen extends StatelessWidget {
+  const MobileSellerScreen({super.key});
+
+  static String routeName = '/seller/dashboard';
+  static String routePath = '/seller/dashboard';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          children: [
+            ElevatedButton(
+                onPressed: () => context.go('/loginPage'),
+                child: const Text('logout ')),
+            const Text('seller mobile interface'),
+          ],
+        ),
+      ),
+    );
+  }
+}
