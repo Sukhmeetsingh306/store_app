f8f28cc235bf42c161439ba48c142bc1cfde4a4c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-09-02 19:42:05 +0530

    feat: fix import and ignore print statement

--- multi_vendor/lib/screen/authentication/seller/button/seller_login_button.dart
+++ multi_vendor/lib/screen/authentication/seller/button/seller_login_button.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../controllers/seller_controllers.dart';
 import '../../../../provider/seller_provider.dart';
