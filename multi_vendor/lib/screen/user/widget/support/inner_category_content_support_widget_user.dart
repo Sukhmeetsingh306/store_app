1525355d587cd1b0b1afed3776a801619d511dc7
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-09-05 19:40:21 +0530

    feat: added the code for the row text to appear at top in the inner screen

--- multi_vendor/lib/screen/user/widget/support/inner_category_content_support_widget_user.dart
+++ multi_vendor/lib/screen/user/widget/support/inner_category_content_support_widget_user.dart
@@ -1,17 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../controllers/subCategory_controllers.dart';
 import '../../../../models/api/category_api_models.dart';
 import '../../../../models/api/subcategory_api_models.dart';
 import '../../../../utils/fonts/google_fonts_utils.dart';
+import '../../../../utils/fonts/row_text_sands.dart';
+import '../../../../utils/widget/platform/platform_check.dart';
 import 'banner_image_support_widget_user.dart';
 import 'sub_category_support_user.dart';
 
 class InnerCategoryContentSupportWidgetUser extends StatefulWidget {
   final CategoryApiModels category;
+  final bool? listView;
 
   const InnerCategoryContentSupportWidgetUser(
-      {super.key, required this.category});
+      {super.key, required this.category, this.listView});
 
   @override
   State<InnerCategoryContentSupportWidgetUser> createState() =>
@@ -48,8 +52,27 @@ class _InnerCategoryContentSupportWidgetUserState
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: SubCategorySupportUser(
-                future: subCategoryModel,
+              child: Column(
+                children: [
+                  SubCategorySupportUser(
+                    future: subCategoryModel,
+                  ),
+                  SizedBox(height: kIsWeb ? 10 : 4),
+                  kIsWeb
+                      ? RowTextSands(
+                          title: 'Popular Product:',
+                          subTitle: ' View All',
+                          mainAxisAlignment:
+                              kIsWeb && (isIOSWeb() || isAndroidWeb())
+                                  ? MainAxisAlignment.spaceBetween
+                                  : MainAxisAlignment.spaceEvenly,
+                        )
+                      : RowTextSands(
+                          title: 'Popular Product:',
+                          subTitle: ' View All',
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        ),
+                ],
               ),
             ),
           ),
