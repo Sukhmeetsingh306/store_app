9647303a7ccf328e4619686bc003f0be2f6e7d28
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-09-05 20:41:04 +0530

    feat: imported the code in the file in which we can use it again

--- multi_vendor/lib/screen/user/widget/support/inner_category_content_support_widget_user.dart
+++ multi_vendor/lib/screen/user/widget/support/inner_category_content_support_widget_user.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_vendor/controllers/product_controllers.dart';
+import 'package:multi_vendor/screen/user/widget/support/reuse_widget_support.dart';
 
 import '../../../../controllers/subCategory_controllers.dart';
 import '../../../../models/api/category_api_models.dart';
 import '../../../../models/api/subcategory_api_models.dart';
+import '../../../../models/product_model.dart';
 import '../../../../utils/fonts/google_fonts_utils.dart';
 import '../../../../utils/fonts/row_text_sands.dart';
 import '../../../../utils/widget/platform/platform_check.dart';
@@ -12,10 +15,13 @@ import 'sub_category_support_user.dart';
 
 class InnerCategoryContentSupportWidgetUser extends StatefulWidget {
   final CategoryApiModels category;
-  final bool? listView;
+  final bool listView;
 
-  const InnerCategoryContentSupportWidgetUser(
-      {super.key, required this.category, this.listView});
+  const InnerCategoryContentSupportWidgetUser({
+    super.key,
+    required this.category,
+    this.listView = false,
+  });
 
   @override
   State<InnerCategoryContentSupportWidgetUser> createState() =>
@@ -25,23 +31,32 @@ class InnerCategoryContentSupportWidgetUser extends StatefulWidget {
 class _InnerCategoryContentSupportWidgetUserState
     extends State<InnerCategoryContentSupportWidgetUser> {
   late Future<List<SubCategoryApiModels>> subCategoryModel;
+  late Future<List<ProductModel>> productModel;
+
   final SubCategoryControllers subCategoryControllers =
       SubCategoryControllers();
+  final ProductController productController = ProductController();
+
+  final Set<int> favoriteIndexes = {};
+  final Set<int> cartIndexes = {};
 
   @override
   void initState() {
     super.initState();
     subCategoryModel = subCategoryControllers
         .getSubCategoryByCategoryName(widget.category.categoryName);
+    productModel =
+        productController.fetchProductCategory(widget.category.categoryName);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
         children: [
           bannerImageResponsiveWidget(context, widget.category.categoryBanner),
+          const SizedBox(height: 10),
           Center(
             child: googleInterText(
               'Shop by Category',
@@ -49,32 +64,47 @@ class _InnerCategoryContentSupportWidgetUserState
               letterSpacing: 1.4,
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Column(
-                children: [
-                  SubCategorySupportUser(
-                    future: subCategoryModel,
-                  ),
-                  SizedBox(height: kIsWeb ? 10 : 4),
-                  kIsWeb
-                      ? RowTextSands(
-                          title: 'Popular Product:',
-                          subTitle: ' View All',
-                          mainAxisAlignment:
-                              kIsWeb && (isIOSWeb() || isAndroidWeb())
-                                  ? MainAxisAlignment.spaceBetween
-                                  : MainAxisAlignment.spaceEvenly,
-                        )
-                      : RowTextSands(
-                          title: 'Popular Product:',
-                          subTitle: ' View All',
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        ),
-                ],
-              ),
-            ),
+          const SizedBox(height: 10),
+          SubCategorySupportUser(future: subCategoryModel),
+          const SizedBox(height: 10),
+          kIsWeb
+              ? RowTextSands(
+                  title: 'Popular Product:',
+                  subTitle: ' View All',
+                  mainAxisAlignment: kIsWeb && (isIOSWeb() || isAndroidWeb())
+                      ? MainAxisAlignment.spaceBetween
+                      : MainAxisAlignment.spaceEvenly,
+                )
+              : const RowTextSands(
+                  title: 'Popular Product:',
+                  subTitle: ' View All',
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                ),
+          futureBuilderProduct(
+            context,
+            productModel,
+            "No Popular Products",
+            widget.listView,
+            favoriteIndexes,
+            cartIndexes,
+            (index) {
+              setState(() {
+                if (favoriteIndexes.contains(index)) {
+                  favoriteIndexes.remove(index);
+                } else {
+                  favoriteIndexes.add(index);
+                }
+              });
+            },
+            (index) {
+              setState(() {
+                if (cartIndexes.contains(index)) {
+                  cartIndexes.remove(index);
+                } else {
+                  cartIndexes.add(index);
+                }
+              });
+            },
           ),
         ],
       ),
