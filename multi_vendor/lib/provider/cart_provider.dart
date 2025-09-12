c5caf39e172de065d241d25cd0121296293b4b62
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-09-13 00:38:29 +0530

    feat: provider added for the cart != null

---
+++ multi_vendor/lib/provider/cart_provider.dart
@@ -0,0 +1,46 @@
+// manage the state of the cart
+// the state of the initial cart
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:multi_vendor/models/chart_model.dart';
+
+class CartProvider extends StateNotifier<Map<String, ChartModel>> {
+  CartProvider() : super({});
+
+  // method to add the product in the cart
+  void addProductToCart({
+    required String productName,
+    required int productPrice,
+    required String productCategory,
+    required List<String> productImage,
+    required String sellerId,
+    required int productQuantity,
+    required int totalQuantity,
+    required String productId,
+    required String productDescription,
+    required String fullName,
+  }) {
+    // check if the cart != null
+    if (state.containsKey(productId)) {
+      state = {
+        ...state,
+        productId: ChartModel(
+//          productName: state[productId]!.productId,
+          productName: state[productId]!.productName,
+          productPrice: state[productId]!.productPrice,
+          productCategory: state[productId]!.productCategory,
+          productImage: state[productId]!.productImage,
+          sellerId: state[productId]!.sellerId,
+          productQuantity: state[productId]!.productQuantity,
+          totalQuantity: state[productId]!.totalQuantity + 1,
+          productId: state[productId]!.productId,
+          productDescription: state[productId]!.productDescription,
+          fullName: state[productId]!.fullName,
+        ),
+      };
+    } else {
+      // if the cart == null
+      // add the product using the provider
+    }
+  }
+}
